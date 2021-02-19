6d1ccbb4f0bb3f00d19aa454bb7808eaa574787e
zoisper <zoisper2gmail.com>
2021-02-19 22:54:25 +0000

    wip

--- treino1.py
+++ treino1.py
@@ -27,6 +27,73 @@ def aloca(prefs):
         l = l + 1
     return list(prefs)
 
+
+'''
+Defina uma função que, dada uma lista de nomes de pessoas, devolva essa lista ordenada 
+por ordem crescente do número de apelidos (todos menos o primeiro nome). No caso de pessoas com o mesmo número de apelidos,
+devem ser listadas por ordem lexicográfica do nome completo.
+'''
+
+def apelidos(nomes):
+    nomes.sort()
+    nomes.sort(key=lambda t: len(t.split()))
+    return nomes
+
+
+
+'''
+Podemos usar um (multi) grafo para representar um mapa de uma cidade: 
+cada nó representa um cruzamento e cada aresta uma rua.
+
+Pretende-se que implemente uma função que lista os cruzamentos de uma cidade 
+por ordem crescente de criticidade: um cruzamento é tão mais crítico quanto 
+maior o número de ruas que interliga.
+
+A entrada consistirá numa lista de nomes de ruas (podendo assumir que os nomes de ruas são únicos). 
+Os identificadores dos cruzamentos correspondem a letras do alfabeto, e cada rua começa (e acaba) no cruzamento 
+identificado pelo primeiro (e último) caracter do respectivo nome.
+
+A função deverá retornar uma lista com os nomes dos cruzamentos por ordem crescente de criticidade, 
+listando para cada cruzamento um tuplo com o respectivo identificador e o número de ruas que interliga.
+Apenas deverão ser listados os cruzamentos que interliguem alguma rua, e os cruzamentos com o mesmo 
+nível de criticidade deverão ser listados por ordem alfabética.
+'''
+
+def cruzamentos(ruas):
+    dic = {}
+    for i in ruas:
+        if i[0] not in dic:
+            dic[i[0]] = 1
+        else:
+            dic[i[0]] +=1
+        if i[-1] not in dic:
+            dic[i[-1]] = 1
+        else:
+            dic[i[-1]] +=1
+    lista = sorted(dic.items())
+    lista.sort(key=lambda t : (t[1], t[0]))
+    return lista
+
+'''
+Defina uma função que recebe um número positivo
+e produz a soma dos seus factores primos distintos.
+'''
+
+def primo(n):
+    for i in range(2, n-1):
+        if n%i == 0:
+            return False
+    return True
+
+def factoriza(n):
+    r = 0
+    for i in range (2, n):
+        if primo(i) and n%i == 0:
+            r += i
+            while n%i == 0:
+                n /= i
+    return r
+
 """
 Neste problem pretende-se que defina uma função que, dada uma string com palavras, 
 devolva uma lista com as palavras nela contidas ordenada por ordem de frequência,
