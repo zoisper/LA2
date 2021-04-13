94baa17f83fd75956ff72d291501f9f0eed3328d
zoisper <zoisper2gmail.com>
2021-04-13 22:57:15 +0100

    vendedor 10%

--- treino3.py
+++ treino3.py
@@ -241,3 +241,50 @@ def validas(soma,listas):
         if aux(soma,lista,size):
             result.append(lista)
     return result
+
+"""
+
+Um vendedor ambulante tem que decidir que produtos levará na sua próxima viagem.
+Infelizmente, tem um limite de peso que pode transportar e, tendo isso em atenção, 
+tem que escolher a melhor combinação de produtos a transportar dentro desse limite 
+que lhe permitirá ter a máxima receita.
+
+Implemente uma função que, dado o limite de peso que o vendedor pode transportar, 
+e uma lista de produtos entre os quais ele pode escolher (assuma que tem à sua 
+disposição um stock ilimitado de cada produto), devolve o valor de receita máximo
+que poderá obter se vender todos os produtos que escolher transportar, e a lista
+de produtos que deverá levar para obter essa receita (incluindo repetições, 
+caso se justifique), ordenada alfabeticamente.
+
+Cada produto consiste num triplo com o nome, o valor, e o peso.
+
+Caso haja 2 produtos com a mesma rentabilidade por peso deverá dar prioridade 
+aos produtos que aparecem primeiro na lista de entrada.
+
+"""
+
+def vendedor(capacidade,produtos):
+    d ={}
+    d[0] = 0
+    saco = {}
+    for cap in range(1, capacidade+1):
+        r = 0
+        for p in produtos:
+            if p[2] <= cap:
+                a = p[1] + d[cap - p[2]]
+                if a > r:
+                    r = a
+                    saco[cap] = p
+        d[cap] = r
+    
+    
+    valor = d[capacidade]
+    lista = []
+    
+    while capacidade:
+        lista.append(saco[capacidade])
+        capacidade -= saco[capacidade][2]
+    lista = [x[0] for x in lista]
+    lista.sort()
+    
+    return (valor,lista)
